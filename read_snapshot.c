92f93e418138f2e573e798c1915c29aec49b6f19
J.M. Dana <jmdana@gmail.com>
2017-03-24 23:02:12 +0000

    copyright notice + license

--- read_snapshot.c
+++ read_snapshot.c
@@ -1,15 +1,33 @@
 
 /*
+ * Copyright (c) 2017 Jose M. Dana
  *
- * Modify both ALLOWED_TAGS and N_ALLOWED_TAGS in order 
- * to cherry-pick the tags for the destination file
+ * This file is part of gadget_snapshots.
+ * http://github.com/jmdana/gadget_snapshots
  *
+ * gadget_snapshots is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation (version 3 of the License only).
+ *
+ * gadget_snapshots is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with gadget_snapshots. If not, see <http://www.gnu.org/licenses/>.
  */
-
 
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+
+/*
+ *
+ * Modify both ALLOWED_TAGS and N_ALLOWED_TAGS in order
+ * to cherry-pick the tags for the destination file
+ *
+ */
 
 #define N_ALLOWED_TAGS  9
 
