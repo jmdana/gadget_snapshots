92d455da174c8384800b6aeea7bc578e242e98e1
J.M. Dana <jmdana@gmail.com>
2017-03-28 00:07:16 +0100

    license header added

--- utils.h
+++ utils.h
@@ -1,3 +1,23 @@
 
+/*
+ * Copyright (c) 2017 Jose M. Dana
+ *
+ * This file is part of gadget_snapshots.
+ * http://github.com/jmdana/gadget_snapshots
+ *
+ * gadget_snapshots is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation (version 3 of the License only).
+ *
+ * gadget_snapshots is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with gadget_snapshots. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+
 int same_file(char *src, char *dst);
 int getonechar();
